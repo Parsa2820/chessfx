c38825eaad8cb9a4316807155b07fe003ba25b41
parsa <parsa2820@gmail.com>
2020-05-29 18:06:09 +0430

    Add game scene

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,17 @@
 package controller;
 
-public class GameController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+    @FXML private TableView blackMoves;
+    @FXML private TableView whiteMoves;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/model/piece/Piece.java
+++ src/main/java/model/piece/Piece.java
@@ -2,6 +2,9 @@ package model.piece;
 
 public abstract class Piece {
 
+    int currentX;
+    int currentY;
+
     abstract boolean canMoveTo();
 
 }
